47e37ce381e410db08615d7a9508b97a70c60bc2
Explosivejoe190 <rogersarran2@gmail.com>
2025-08-17 14:12:46 +0100

    oops

--- kubejs/server_scripts/integratedDynamics.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes(event => {
-    //STOP DOXXING GRRRRRRR
-    event.remove({ output: 'integrateddynamics:part_entity_reader' })
-    event.remove({ output: 'integrateddynamics:part_world_reader' })
-    event.remove({ output: 'integrateddynamics:part_extradimensional_reader' })
-})
